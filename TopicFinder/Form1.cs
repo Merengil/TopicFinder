9d3d22ed8be8d786f0b79ac31487d4e1bb636c0b
Merengil <ARdownloader@hotmail.com>
2024-08-15 11:58:03 +0200

    Add project files.

---
+++ TopicFinder/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopicFinder
+{
+    public partial class Form1 : Form
+    {
+        List<string> listTopics;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random(DateTime.Now.Millisecond);
+            listTopics.
+        }
+    }
+}
